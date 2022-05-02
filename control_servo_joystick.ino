7c01f7f38ac440fa36313761d7b041ef1f9915c8
Cesar Alejandro Ramírez Alcaraz <66204687+cesaralejandroramirezalcaraz@users.noreply.github.com>
2022-05-01 20:48:31 -0500

    delete extra information

--- control_servo_joystick.ino
+++ control_servo_joystick.ino
@@ -1,19 +1,19 @@
 #include <Servo.h>
 Servo servoMotor;
 int xPin = A0;
-int yPin = A1;
+
 
 int contador1 = 0;
 int xPosition = 0;
 int yPosition = 0;
-int buttonState = 0;
+
 
 void setup()
 {
 
   Serial.begin(9600);
   pinMode(xPin, INPUT);
-  pinMode(yPin, INPUT);
+
   servoMotor.attach(8);
  
 }
@@ -38,8 +38,6 @@ void loop()
   Serial.print(contador1);
   Serial.print("X: ");
   Serial.print(xPosition);
-  Serial.print(" | Y: ");
-  Serial.print(yPosition);
-  Serial.print(" | Button: ");
+ 
 
 }
